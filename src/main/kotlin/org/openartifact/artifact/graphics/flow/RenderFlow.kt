7a6d462b1710292ad12d0a758c63e006b3c317c1
meo209 <redslyice@outlook.de>
2024-05-12 21:03:58 +0200

    fix: RenderFlow directCommit methods, block execution. Sandbox shader fix

--- src/main/kotlin/org/openartifact/artifact/graphics/flow/RenderFlow.kt
+++ src/main/kotlin/org/openartifact/artifact/graphics/flow/RenderFlow.kt
@@ -15,13 +15,54 @@ class RenderFlow {
      * Commits a graphics component to the rendering flow.
      *
      * @param element The graphics component to commit.
+     * @return The committed graphics component.
      */
-    fun commit(element : IGraphicsComponent) =
+    fun <T : IGraphicsComponent> commit(element : T) : T {
         committedElements.add(element)
-
+        return element
+    }
 
     /**
-     * Pushes all commits to the render pipeline
+     * Commits a graphics component to the rendering flow and executes a block of code on it.
+     *
+     * @param element The graphics component to commit.
+     * @param block The block of code to execute on the committed graphics component.
+     * @return The committed graphics component.
+     */
+    fun <T : IGraphicsComponent> commit(element : T, block : T.() -> Unit) : T {
+        block(element)
+        commit(element)
+        return element
+    }
+
+    /**
+     * Directly commits and pushes an element.
+     * NOTE: Should be avoided if possible. Can lead to unexpected behavior.
+     *
+     * @param element The graphics component to commit and push.
+     * @return The committed and pushed graphics component.
+     */
+    fun <T : IGraphicsComponent> directCommit(element : T) : T {
+        element.push()
+        return element
+    }
+
+    /**
+     * Directly commits and pushes an element and executes a block of code on it.
+     * NOTE: Should be avoided if possible. Can lead to unexpected behavior.
+     *
+     * @param element The graphics component to commit and push.
+     * @param block The block of code to execute on the committed and pushed graphics component.
+     * @return The committed and pushed graphics component.
+     */
+    fun <T : IGraphicsComponent> directCommit(element : T, block : T.() -> Unit) : T {
+        directCommit(element)
+        block(element)
+        return element
+    }
+
+    /**
+     * Pushes all committed elements to the render pipeline.
      */
     fun push() {
         require(committedElements.size <= MAX_COMMITS) { "Exceeded maximum number of commits. Please remove some committed elements or adjust MAX_COMMITS." }
@@ -32,7 +73,7 @@ class RenderFlow {
     }
 
     /**
-     * Commits all [elements] to the [committedElements]
+     * Commits multiple graphics components to the rendering flow.
      *
      * @param elements The graphics components to commit.
      */
@@ -45,7 +86,7 @@ class RenderFlow {
  * Creates and executes a rendering flow using the provided block of code.
  *
  * @param block The block of code to execute within the rendering flow context.
- * @return The created org.openartifact.artifact.graphics.flow.RenderFlow instance after executing the block.
+ * @return The created [RenderFlow] instance after executing the block.
  */
 fun renderFlow(block : RenderFlow.() -> Unit) : RenderFlow {
     val flow = RenderFlow()
@@ -53,4 +94,5 @@ fun renderFlow(block : RenderFlow.() -> Unit) : RenderFlow {
     return flow
 }
 
+// Maximum number of commits allowed
 var MAX_COMMITS = 320000
