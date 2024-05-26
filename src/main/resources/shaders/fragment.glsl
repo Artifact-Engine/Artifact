d753610f1f4f44bd651d18634dc5fd3f44178b27
meo209 <redslyice@outlook.de>
2024-05-26 10:09:24 +0200

    feat: Basic Lighting

--- src/main/resources/shaders/fragment.glsl
+++ src/main/resources/shaders/fragment.glsl
@@ -1,11 +1,24 @@
 #version 330 core
 
-layout(location = 0) out vec4 color;
+layout(location = 0) out vec4 a_Color;
 
 in vec3 v_Color;
 in vec3 v_Normal;
-in vec3 v_FragPosition;
+in vec3 v_Position;
+
+uniform vec3 u_Light_Pos;
+uniform vec3 u_Light_Color;
 
 void main() {
-    color = vec4(v_Color, 1.0);
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * u_Light_Color;
+
+    vec3 norm = normalize(v_Normal);
+    vec3 lightDir = normalize(u_Light_Pos - v_Position);
+
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff + u_Light_Color;
+
+    vec3 result = (ambient + diffuse) * v_Color;
+    a_Color = vec4(result, 1.0);
 }
